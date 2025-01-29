97d1704ae6f45d52f1ba947f33f3495b337eb43b
Charley Lea <charleylea@att.net>
2025-01-28 21:57:54 -0500

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import { useState } from "react";
+import { puppyList } from "./data.js";
+
 function App() {
-  return <div></div>;
+  const [puppies, setPuppies] = useState(puppyList);
+
+  console.log("puppyList: ", puppyList);
+
+  return (
+    <div className="App">
+      {puppies.map((puppy) => {
+        return <p key={puppy.id}>{puppy.name}</p>;
+      })}
+    </div>
+  );
 }
 
 export default App;
